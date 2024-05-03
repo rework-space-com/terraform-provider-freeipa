8c78eaf811529f7598e2cebed1ad363bdfa975dd
RomanButsiy <butsiyroman@gmail.com>
2024-05-03 11:10:58 +0300

    Fix import

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	
-	"github.com/rework-space-com/terraform-provider-freeipa/freeipa"
+	"terraform-provider-freeipa/freeipa"
 )
 
 func main() {
